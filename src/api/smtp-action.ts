bd3daa3d4584aa274e41f12e8b8b81a5c694c057
Alex-Fes <aleksei.fesenko86@gmail.com>
2025-01-21 19:59:13 +0100

    fix endpoint

--- src/api/smtp-action.ts
+++ src/api/smtp-action.ts
@@ -2,5 +2,5 @@ import {instance} from "./instance";
 import {FeedbackValuesType} from "../types/contact-form-type";
 
 export const sendEmail = async (values: FeedbackValuesType) => {
-  return instance.post('/sendMessage', values)
+  return instance.post('/send-email', values)
 }
