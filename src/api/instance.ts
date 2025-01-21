a4790b5bdb03d7345dda1282e5e7f871d46d1a40
Alex-Fes <aleksei.fesenko86@gmail.com>
2025-01-21 19:35:59 +0100

    fix baseURL

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -5,10 +5,12 @@ import axios from 'axios'
 // mail-smpt-matveys-projects-86ea41cd.vercel.app
 
 export const instance = axios.create({
-  baseURL: `http://mail-smpt.vercel.app`,
+  baseURL: `https://mail-smpt.vercel.app`,
   withCredentials: true,
   headers: {
     Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   },
 })
 
